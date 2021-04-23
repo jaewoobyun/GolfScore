4907064060e57c0307ca3f632908ef54d30985d6
Jaewoo Byun <jaewoobyun1@gmail.com>
2021-04-23 17:04:43 +0900

    some changes

--- GolfScore/Model/Course+CoreDataProperties.swift
+++ GolfScore/Model/Course+CoreDataProperties.swift
@@ -17,5 +17,23 @@ extension Course {
     }
 
     @NSManaged public var name: String?
-
+    @NSManaged public var hole1: Int16?
+    @NSManaged public var hole2: Int16?
+    @NSManaged public var hole3: Int16?
+    @NSManaged public var hole4: Int16?
+    @NSManaged public var hole5: Int16?
+    @NSManaged public var hole6: Int16?
+    @NSManaged public var hole7: Int16?
+    @NSManaged public var hole8: Int16?
+    @NSManaged public var hole9: Int16?
+    @NSManaged public var hole10: Int16?
+    @NSManaged public var hole11: Int16?
+    @NSManaged public var hole12: Int16?
+    @NSManaged public var hole13: Int16?
+    @NSManaged public var hole14: Int16?
+    @NSManaged public var hole15: Int16?
+    @NSManaged public var hole16: Int16?
+    @NSManaged public var hole17: Int16?
+    @NSManaged public var hole18: Int16?
+    
 }
